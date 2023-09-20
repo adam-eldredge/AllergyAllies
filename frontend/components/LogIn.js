f65b793984af3d5589c3b1679fed0b9d6b515ed7
alexaajello <62025094+alexaajello@users.noreply.github.com>
2023-09-20 13:07:16 -0400

    Update LogIn.js

--- frontend/components/LogIn.js
+++ frontend/components/LogIn.js
@@ -21,13 +21,13 @@ class LogIn extends Component {
          <Text style = {styles.title}>Allergy Ally</Text>
             <TextInput style = {styles.input}
                underlineColorAndroid = "transparent"
-               placeholder = "  Email"
+               placeholder = "Email"
                autoCapitalize = "none"
                onChangeText = {this.handleEmail}/>
             
             <TextInput style = {styles.input}
                underlineColorAndroid = "transparent"
-               placeholder = "  Password"
+               placeholder = "Password"
                autoCapitalize = "none"
                onChangeText = {this.handlePassword}/>
             
@@ -39,9 +39,21 @@ class LogIn extends Component {
                <Text style = {styles.logInButtonText}> Log In </Text>
             </TouchableOpacity>
 
-            <Text style = {styles.bottomText}>Forgot password?</Text>
-            <Text style = {styles.bottomText}>Sign up as a patient</Text>
-            <Text style = {styles.bottomText}>Sign up as a medical practice</Text>
+            <TouchableOpacity>
+               <Text style = {styles.bottomText}>Forgot Password?</Text>
+            </TouchableOpacity>
+
+            <Text style = {styles.bottomText2}>Don't have an account yet?</Text>
+            <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center'}}>
+            <TouchableOpacity
+               style = {styles.signUpButton}>
+               <Text style = {styles.logInButtonText}>I am a patient</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+               style = {styles.signUpButton}>
+               <Text style = {styles.logInButtonText}>I am a practice</Text>
+            </TouchableOpacity>
+            </View>
          </View>
       )
    }
@@ -61,26 +73,44 @@ const styles = StyleSheet.create({
    input: {
       margin: 15,
       height: 40,
-      borderColor: '#5791F3',
+      borderColor: '#1059d5',
       borderWidth: 1,
+      padding: 10
    },
    logInButton: {
-      backgroundColor: '#5791F3',
+      backgroundColor: '#1059d5',
       padding: 10,
       margin: 15,
       height: 40,
       justifyContent: 'center',
-      marginBottom: 30,
+      borderRadius: 8,
    },
    logInButtonText:{
       color: 'white',
-      textAlign: 'center'
+      textAlign: 'center',
+      fontSize: 15,
    },
    bottomText:{
       color: '#1059d5',
       marginLeft: 15,
-      marginBottom: 5,
+      marginBottom: 30,
       fontSize: 17,
       textAlign: 'center',
+   },
+   bottomText2:{
+      color: '#606060',
+      marginLeft: 15,
+      fontSize: 17,
+      textAlign: 'center',
+      fontWeight: '600'
+   },
+   signUpButton: {
+      backgroundColor: '#1059d5',
+      padding: 10,
+      margin: 10,
+      height: 40,
+      width: 150,
+      justifyContent: 'center',
+      borderRadius: 8,
    },
 })
