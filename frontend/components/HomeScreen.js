36561877d6b730920931db39ef390288c95ef65e
alexaajello <62025094+alexaajello@users.noreply.github.com>
2023-09-20 13:07:02 -0400

    Update HomeScreen.js

--- frontend/components/HomeScreen.js
+++ frontend/components/HomeScreen.js
@@ -7,18 +7,21 @@ export default function HomeScreen({navigation}){
        <View>
         <Button
         title="Alerts"
+        color="#1059d5"
         onPress={() =>
           navigation.navigate('Alerts')
         }
       />
         <Button
         title="Login"
+        color="#1059d5"
         onPress={() =>
           navigation.navigate('Login')
         }
       />
       <Button
         title="Portal"
+        color="#1059d5"
         onPress={() =>
           navigation.navigate('Portal')
         }
