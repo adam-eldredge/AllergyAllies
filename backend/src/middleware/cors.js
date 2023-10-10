0a4e37464cb9fc1682b585ef7f29a7cb5f6bdb2c
JimCDC <56973149+jimcDC@users.noreply.github.com>
2023-10-09 20:45:07 -0400

    Provider sign up is working now

--- backend/src/middleware/cors.js
+++ backend/src/middleware/cors.js
@@ -4,11 +4,7 @@ const corsOptions = {
   origin: 'http://localhost:19006', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
-  optionsSuccessStatus: 204,
-
-  // origin: 'https://npiregistry.cms.hhs.gov/api',
-  // methods: 'GET',
-  // credentials: true
+  optionsSuccessStatus: 204
 };
 
 module.exports = cors(corsOptions);
