28525072a9e4c7b5db3713f562a4b18b5a09ddd3
Adam Eldredge <adamteldredge@gmail.com>
2023-10-12 16:47:23 -0400

    pushing

--- frontend/screens/Survey/PracticeSurvey.js
+++ frontend/screens/Survey/PracticeSurvey.js
@@ -1,17 +1,18 @@
 import React, { useContext, useState } from 'react';
-import { View, Text, TextInput, Button, Dimensions, StyleSheet, TouchableOpacity, Alert } from 'react-native';
+import { Platform } from 'react-native';
 import { useRoute } from '@react-navigation/native';
-import { AuthContext, AuthProvider } from '../../App.js'
 import axios from 'axios';
 import 'survey-core/defaultV2.min.css';
 import { Model } from 'survey-core';
 import { Survey } from 'survey-react-ui';
 import theme from './theme.js';
+import AuthContext from '../../AuthContext';
+import jwt_decode from 'jwt-decode';
 
 // ***** SURVEY QUESTIONS ***** //
 const surveyJson = {
     title: "Allergy Allies - Initial Practice Survey",
-    description: "Please fill out all fields to finish setting up your practice!",
+    description: `Welcome , Please fill out all fields to finish setting up your practice!`,
     textAlign: "center",
     pages:
         [
@@ -24,31 +25,28 @@ const surveyJson = {
                             name: 'a',
                             title: 'Practice Name',
                             type: 'text',
-                            isRequired: 'true'
+
                         },
                         {
                             name: 'b',
                             title: 'Treating Physician/Provider Name',
-                            type: 'dropdown',
-                            isRequired: 'true'
+                            type: 'dropdown'
                         },
                         {
                             name: 'c',
                             title: 'Practice Logo',
                             type: 'file',
-                            isRequired: 'true'
                         },
                         {
                             name: 'd',
                             title: 'Practice Scrolling Ads',
                             type: 'file',
-                            isRequired: 'true'
                         },
                         {
                             name: 'e',
                             title: 'Practice Address',
                             type: 'text',
-                            isRequired: 'true'
+
                         }
                     ],
                 showQuestionNumbers: 'false'
@@ -62,7 +60,6 @@ const surveyJson = {
                             name: 'f',
                             title: 'Starting frequency of injections',
                             type: 'radiogroup',
-                            isRequired: 'true',
                             colCount: '3',
                             choices: ['1', '2', '3'],
                             seperateSpecialChoices: 'true',
@@ -70,7 +67,6 @@ const surveyJson = {
                             name: 'g',
                             title: 'Frequency of Clinical Follow-up Appointments',
                             type: 'radiogroup',
-                            isRequired: 'true',
                             colCount: '4',
                             choices: ['Once yearly', 'Twice yearly', 'Thrice yearly', '4x yearly'],
                             seperateSpecialChoices: 'true'
@@ -78,7 +74,6 @@ const surveyJson = {
                             name: 'h',
                             title: 'Frequency of Progress Self-Assessment',
                             type: 'radiogroup',
-                            isRequired: 'true',
                             colCount: '4',
                             choices: ['Once yearly', 'Twice yearly', 'Thrice yearly', '4x yearly'],
                             seperateSpecialChoices: 'true'
@@ -97,7 +92,6 @@ const surveyJson = {
                             panelCount: '1',
                             maxPanelCount: '10',
                             confirmDelete: 'true',
-                            isRequired: 'true',
                             templateElements: [{
                                 name: 'i1',
                                 title: 'Antigen',
@@ -115,7 +109,6 @@ const surveyJson = {
                             panelCount: '1',
                             maxPanelCount: '10',
                             confirmDelete: 'true',
-                            isRequired: 'true',
                             templateElements: [{
                                 name: 'j1',
                                 title: 'Name of treatment vial',
@@ -142,7 +135,6 @@ const surveyJson = {
                                 name: 'k1',
                                 title: 'Automatic',
                                 type: 'radiogroup',
-                                isRequired: 'true',
                                 colCount: '3',
                                 choices: ['Default', 'Customize', 'Disable'],
                                 seperateSpecialChoices: 'true'
@@ -156,7 +148,6 @@ const surveyJson = {
                                 min: '0',
                                 max: '0.50',
                                 default: '0.05',
-                                isRequired: 'true',
                                 visibleIf: "{k1} = 'Customize'"
                             }
                             ]
@@ -165,26 +156,26 @@ const surveyJson = {
                             name: 'l',
                             title: 'Dose Adjustments',
                             type: 'text',
-                            isRequired: 'true'
+
                         }, {
                             name: 'm',
                             title: 'General Rules for Adjustments',
                             type: 'text',
-                            isRequired: 'true'
+
                         }, {
                             name: 'n',
                             title: 'Dose Adjustments Defaults',
                             type: 'text',
-                            isRequired: 'true'
+
                         }
                     ]
             }
         ],
-        showTOC: 'true',
-        completeText: 'Submit',
-        showPreviewBeforeComplete: 'showAllQuestions',
-        showQuestionNumbers: 'false',
-        questionErrorLocation: 'bottom',
+    showTOC: 'true',
+    completeText: 'Submit',
+    showPreviewBeforeComplete: 'showAllQuestions',
+    showQuestionNumbers: 'false',
+    questionErrorLocation: 'bottom',
 };
 
 export default function PracticeSurvey() {
@@ -192,5 +183,5 @@ export default function PracticeSurvey() {
     const survey = new Model(surveyJson);
     survey.applyTheme(theme);
 
-    return (<Survey model={survey} />);
+    return (<Survey model={survey} />)
 }
