e15385265a94972d945fd8b42d637d17fe9ce9b8
JimCDC <56973149+jimcDC@users.noreply.github.com>
2023-10-31 22:50:21 -0400

    Removed hard coded antigens in determining next treatment, need to work on adding single treatments

--- backend/src/Models/treatment.js
+++ backend/src/Models/treatment.js
@@ -25,28 +25,17 @@ const dataSchema = new mongoose.Schema({
         type: String
     },
 
-    //These may be redundant or can replace medication
-    // vialNames: {pollen: Boolean, insectsAndPets: Boolean, molds: Boolean},
-    // medication: {
-    //     required: true,
-    //     type: String
-    // },
+    bottles: {
+        required: false,
+        type: [{
+            nameOfBottle: String,
+            injVol: Number,
+            injDilution: Number,
+            injLLR: Number,
+            currBottleNumber: String,
+        }],
+    },
 
-    //These may be redundant or can replace dosage
-    injVols: {pollenInj: Number, insectsAndPetsInj: Number, moldsInj: Number},
-    // dosage: {
-    //     required: true,
-    //     type: String
-    // },
-
-    dilutions: {pollenDil: Number, insectsAndPetsDil: Number, moldsDil: Number},
-
-    //Store as number or M for maintenance
-    bottleNumbers: {pollenBottleNumber: String, insectsAndPetsBottleNumber: String, moldsBottleNumber: String},
-
-
-    //These values needs to stay updated with each treatment TODO
-    LLR: { pollenLLR: Number, insectsAndPetsLLR: Number, moldsLLR: Number},
     lastVialTests: {
         type: Map,
         of: new mongoose.Schema({
