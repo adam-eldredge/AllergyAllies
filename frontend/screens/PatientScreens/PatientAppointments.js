04acd04c703667adb8d5829241b31d3a3fc3c508
oliviafeller <ofeller@ufl.edu>
2023-12-04 00:20:37 -0500

    changed home screen to have num days until deadline

--- frontend/screens/PatientScreens/PatientAppointments.js
+++ frontend/screens/PatientScreens/PatientAppointments.js
@@ -22,6 +22,8 @@ export default function PatientAppointments({navigation}){
     const [loading, setLoading] = useState(true);
     const [activeSlide, setActiveSlide] = useState(0);
     const [futureAppointmentDeadline, setFutureAppointmentDeadline] = useState(null);
+    const [daysUntilDeadline, setDaysUntilDeadline] = useState(0);
+    const [deadlineOverdue, setDeadlineOverdue] = useState(false)
 
     //function that takes in a date from MongoDB and converts it into readable format
     //includes day of week
@@ -46,15 +48,15 @@ export default function PatientAppointments({navigation}){
     const findPatient = async () => {
       if (email){
         //replace with your IP address, find quickly from "Metro waiting on exp://<ip>:port" under QR code
-        const patientObj = await axios.get(`http:///192.168.0.160:5000/api/findPatient/${email}`)
+        const patientObj = await axios.get(`http://172.20.10.3:5000/api/findPatient/${email}`)
         setPatient(patientObj.data)
       }
     }
-    if (!patient) {console.log("can't find patient"); findPatient(); }
+    if (!patient) {console.log("Patient home screen can't find patient"); findPatient(); }
 
     const findTreatments = async () => {
         //replace with your IP address, find quickly from "Metro waiting on exp://<ip>:port" under QR code
-        const treatmentsObj = await axios.get(`http:///192.168.0.160:5000/api/getAllTreatmentsByID/${patient._id}`)
+        const treatmentsObj = await axios.get(`http://172.20.10.3:5000/api/getAllTreatmentsByID/${patient._id}`)
         
        
 
@@ -71,8 +73,40 @@ export default function PatientAppointments({navigation}){
 
       const treatmentNotAttended = treatmentsObj.data.filter(treatment => treatment.attended === false);
         if (treatmentNotAttended.length === 1) {
+
+          //set appointment deadline
           setFutureAppointmentDeadline(formatDateWithDay(treatmentNotAttended[0].date));
           console.log('Date of future appointment:', futureAppointmentDeadline);
+
+          //get days until appointment deadline
+          const today = new Date(); // current date
+          const todayUtc = new Date(today.getTime() + today.getTimezoneOffset() * 60 * 1000);
+
+          const appointmentDeadline = new Date(treatmentNotAttended[0].date);
+          const deadlineUtc = new Date(appointmentDeadline.getTime() + appointmentDeadline.getTimezoneOffset() * 60 * 1000);
+          
+          // set both dates to the same time (midnight) to ensure accurate day calculation
+          deadlineUtc.setUTCHours(0, 0, 0, 0);
+          todayUtc.setUTCHours(0, 0, 0, 0);
+          console.log(deadlineUtc)
+          console.log(todayUtc)
+          // Calculate the difference in milliseconds between today and the appointment deadline
+          const timeDifference = deadlineUtc.getTime() - todayUtc.getTime();
+          console.log(timeDifference)
+
+          if (timeDifference < 0) {
+            //deadline overdue
+            // calculate the positive difference (days passed since deadline)
+            setDaysUntilDeadline(Math.ceil(-timeDifference / (1000 * 3600 * 24)));
+            console.log('Days until deadline:', daysUntilDeadline)
+            setDeadlineOverdue(true); 
+          }
+            else {
+            //deadline in future
+            setDaysUntilDeadline(Math.ceil(timeDifference / (1000 * 3600 * 24)));
+          }
+        
+
         } else if (treatmentNotAttended.length > 1) {
           setFutureAppointmentDeadline("More than one future appointment found")
           throw new Error('More than one not attended appointment found.');
@@ -113,35 +147,46 @@ export default function PatientAppointments({navigation}){
         var compliance  = "-"
         msg = "No compliance rate available yet."   
     }
-    
-   
 
+
+    var deadlineMsg = "";
+    var deadlineMsg2 = "";
+    var deadlineMsg3 = "";
+    if (deadlineOverdue) {
+      //appointment deadline overdue
+      deadlineMsg = `Appointment deadline was`;
+      deadlineMsg2 = `${daysUntilDeadline} day${daysUntilDeadline !== 1 ? 's' : ''} ago`
+      deadlineMsg3 = 'Come in as soon as \n possible!'
+      deadlineTextStyle = styles.overdue;
+    } else {
+      //appointment deadline in future
+      deadlineMsg = `Appointment deadline in`;
+      deadlineMsg2 = `${daysUntilDeadline} day${daysUntilDeadline !== 1 ? 's' : ''}`
+      deadlineTextStyle = styles.days;
+    }
 
       return (
 
         <ScrollView style = {styles.container}>
         <Text style = {styles.welcome}>Welcome, {firstName}</Text>
-        <TouchableOpacity style = {styles.card} onPress={() =>
-            navigation.navigate('Progress')
-          }>
+        <View style = {styles.card}>
         <Text style = {styles.title1}>You have been</Text>
         <Text style = {styles.complianceNum}>{compliance}%</Text>
         
         <Text style = {styles.title1}>compliant with your treatment schedule.</Text>
-        <Text style = {styles.message}>{msg}</Text>
-        
-        
-        </TouchableOpacity>
-        <Text style = {styles.welcome}>Next appointment deadline:</Text>
+        <Text style = {styles.message}>{msg}</Text> 
+        </View>
 
-        <TouchableOpacity style = {styles.appointment}
-onPress={() =>
+        <TouchableOpacity style = {styles.card2} onPress={() =>
     navigation.navigate('Upcoming')
   }>
-    <Text style={styles.appointmentText}>{futureAppointmentDeadline}</Text>
-</TouchableOpacity>
+        <Text style = {styles.subtext}>{deadlineMsg}</Text>
+        <Text style = {deadlineTextStyle}>{deadlineMsg2}</Text>
+        <Text style = {styles.subDays}>{futureAppointmentDeadline}</Text>
+        <Text style = {styles.overdueSubtext}>{deadlineMsg3}</Text>
+        </TouchableOpacity>
 
-<Image style={{ width: "150%", height: "20%", marginTop: 150, marginLeft: -85}} source={require('../AdPlaceholder.png')} />
+<Image style={{ width: "150%", height: "20%", marginTop: 10, marginLeft: -85, marginBottom: 150}} source={require('../AdPlaceholder.png')} />
 
     </ScrollView>
 
@@ -165,9 +210,24 @@ onPress={() =>
      marginTop: 18,
      marginBottom: 30,
      padding: 12,
- 
- 
     },
+    card2: {
+      alignItems: 'center',
+      borderRadius: 160,
+      height: 320,
+      width: 320,
+      alignSelf: 'center',
+      justifyContent: 'center',
+      //shadowOpacity: 0.1,
+      //shadowOffset: {width: 2, height: 4},
+      //shadowColor: 'black',
+      //shadowOpacity: 0.05,
+      //shadowRadius: 8,
+      backgroundColor: '#0d3375',
+      marginTop: 2,
+      marginBottom: 30,
+      padding: 12,
+     },
     welcome :{
      fontSize: 20,
      fontWeight: '600',
@@ -183,6 +243,25 @@ onPress={() =>
      fontStyle: 'italic',
      color: '#3d3d3d'
      },
+     subtext :{
+      marginHorizontal: 8,
+      marginTop: 40,
+      marginBottom: 5,
+      textAlign: 'center',
+      fontSize: 18,
+      fontWeight: '400',
+      fontStyle: 'italic',
+      color: 'white'
+      },
+      overdueSubtext :{
+        marginHorizontal: 8,
+        marginVertical: 5,
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: '400',
+        fontStyle: 'italic',
+        color: '#ff7661'
+        },
      message :{
          marginHorizontal: 8,
          marginVertical: 5,
@@ -191,6 +270,14 @@ onPress={() =>
          fontWeight: '600',
          color: '#539CF5'
          },
+         message2 :{
+          marginHorizontal: 8,
+      marginVertical: 5,
+      textAlign: 'center',
+      fontSize: 18,
+      fontWeight: '400',
+      color: '#539CF5'
+          },
      title2 :{
          marginBottom: 15,
          textAlign: 'center',
@@ -220,6 +307,34 @@ onPress={() =>
          margin: 10
  
      },
+     days: {
+      //alignSelf: 'center',
+      //marginTop: 50,
+      textAlign: 'center',
+      fontSize: 32,
+      fontWeight: '600',
+      color: 'white',
+      marginBottom: 10
+  },
+  overdue: {
+    //alignSelf: 'center',
+    //marginTop: 50,
+    textAlign: 'center',
+    fontSize: 32,
+    fontWeight: '600',
+    color: '#ff7661',
+    marginBottom: 10
+},
+  subDays: {
+    //alignSelf: 'center',
+    //marginTop: 50,
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '600',
+    color: '#688ab3',
+    marginBottom: 10
+
+},
      subtitle :{
          paddingBottom: 15,
          textAlign: 'center',
